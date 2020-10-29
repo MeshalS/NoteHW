d382260c18009dc3f9a7a10d9571dead79af18b1
MeshalS <meshal.saus@gmail.com>
2020-10-29 20:12:31 +0300

    adding servver things

---
+++ Develop/public/server.js
@@ -0,0 +1,9 @@
+/// express things
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
