ca0ef1dbe1cb7416ab6e12cc5fbe3bcefb4dc969
MeshalS <meshal.saus@gmail.com>
2020-10-29 20:13:13 +0300

    adding objects

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -7,3 +7,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+let notes = [
+    {
+      title: 'hello',
+      text: "test note from server",
+      id: 1
+    }
+  ];
